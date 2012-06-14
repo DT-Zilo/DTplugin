3130ebf43b785614a88ebe8ff5aa0a8ae9313897
DT-Zilo <dtgamestudio.zilo@gmail.com>
2012-06-14 08:22:02 +0700

    rewrite Code

--- src/me/zilo/DTcmd/DTmain.java
+++ src/me/zilo/DTcmd/DTmain.java
@@ -1,34 +1,23 @@
 package me.zilo.DTcmd;
 
-import me.zilo.DTcmd.cmdStore.Puppet_Cmd;
-import me.zilo.DTcmd.cmdStore.fakeOP_Cmd;
+import me.zilo.DTcmd.cmdStore.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class DTmain extends JavaPlugin
 {
-    private Puppet_Cmd pupCmd;
-    private fakeOP_Cmd fOpCmd;
+    private CmdExe cmdExe;
     
     @Override
     public void onEnable()
     {
-        pupCmd = new Puppet_Cmd(2, -1);
-        fOpCmd = new fakeOP_Cmd(1, -1);
+        cmdExe = new CmdExe();
     }
     
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args)
     {
-        if (cmd.getName().equalsIgnoreCase("puppet") || cmd.getName().equalsIgnoreCase("pup"))
-        {
-            return pupCmd.RunCmd(sender, args);
-        }
-        else if (cmd.getName().equalsIgnoreCase("fakeop") || cmd.getName().equalsIgnoreCase("fop"))
-        {
-            return fOpCmd.RunCmd(sender, args);
-        }
-        return true;
+        return cmdExe.CheckCmd(sender, cmd, commandLabel, args);
     }
 }
---
+++ src/me/zilo/DTcmd/cmdStore/CmdExe.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me.zilo.DTcmd.cmdStore;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+/**
+ *
+ * @author Zilo
+ */
+public class CmdExe 
+{
+    private Puppet_Cmd pupCmd;
+    private fakeOP_Cmd fOpCmd;
+    
+    public CmdExe()
+    {
+        pupCmd = new Puppet_Cmd(2, -1);
+        fOpCmd = new fakeOP_Cmd(1, -1);
+    }
+    
+    public boolean CheckCmd(CommandSender sender, Command cmd, String commandLabel, String[] args)
+    {
+        if (cmd.getName().equalsIgnoreCase("puppet") || cmd.getName().equalsIgnoreCase("pup"))
+        {
+            return pupCmd.RunCmd(sender, args);
+        }
+        else if (cmd.getName().equalsIgnoreCase("fakeop") || cmd.getName().equalsIgnoreCase("fop"))
+        {
+            return fOpCmd.RunCmd(sender, args);
+        }
+        return true;
+    }
+}
