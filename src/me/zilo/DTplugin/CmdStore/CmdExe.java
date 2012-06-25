d26df4c33c6201b288bba7f5b7173add4157926e
DT-Zilo <dtgamestudio.zilo@gmail.com>
2012-06-25 09:46:44 +0700

    change perm from DTcmd.xxx to DTplugin.xxx

--- src/me/zilo/DTplugin/CmdStore/CmdExe.java
+++ src/me/zilo/DTplugin/CmdStore/CmdExe.java
@@ -4,6 +4,7 @@
  */
 package me.zilo.DTplugin.CmdStore;
 
+import me.zilo.DTplugin.Listener.AdminChatChannel;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
@@ -16,12 +17,14 @@ public class CmdExe
     private Puppet_Cmd pupCmd;
     private FOP_Cmd fOpCmd;
     private AdminChat_Cmd admChat;
+    private AdminChannel_Cmd admChan;
     
     public CmdExe()
     {
         pupCmd = new Puppet_Cmd(2, -1);
         fOpCmd = new FOP_Cmd(1, 1);
         admChat = new AdminChat_Cmd(1, -1);
+        admChan = new AdminChannel_Cmd(0, 0);
     }
     
     public boolean CheckCmd(CommandSender sender, Command cmd, String commandLabel, String[] args)
@@ -34,9 +37,13 @@ public class CmdExe
         {
             return fOpCmd.RunCmd(sender, args);
         }
-        else if (cmd.getName().equalsIgnoreCase("adminchat") || cmd.getName().equalsIgnoreCase("ad"))
+        else if (cmd.getName().equalsIgnoreCase("adminchat") || cmd.getName().equalsIgnoreCase("adc"))
         {
             return admChat.RunCmd(sender, args);
+        }
+        else if (cmd.getName().equalsIgnoreCase("togglechannel") || cmd.getName().equalsIgnoreCase("tc"))
+        {
+            return admChan.RunCmd(sender, args);
         }
         return true;
     }
